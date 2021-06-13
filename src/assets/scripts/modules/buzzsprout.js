e74cd9cc7ac4176e998d1bb957e463102cb6aa35
John Puddephatt <john@jdp.org.uk>
2021-06-13 22:56:23 +0100

    placeholder and podcast

--- src/assets/scripts/modules/buzzsprout.js
+++ src/assets/scripts/modules/buzzsprout.js
@@ -1,16 +1,59 @@
-window.buzzsprout = function() {
-  return {
-    podcasts: null,
-    isLoading: false,
+window.buzzsprout = function () {
+    return {
+        podcasts: null,
+        isLoading: false,
+        isPlaying: false,
+        currentTrack: null,
 
-    fetchPlaylist() {
-      this.isLoading = true;
-      fetch(`https://eastmarshunited.org/.netlify/functions/buzzsprout`)
-        .then(res => res.json())
-        .then(data => {
-          this.isLoading = false;
-          this.podcasts = data;
-        });
+        fetchPlaylist() {
+            this.isLoading = true
+            fetch(`/.netlify/functions/buzzsprout`)
+                .then((res) => res.json())
+                .then((data) => {
+                    this.isLoading = false
+                    this.podcasts = data.reverse()
+                })
+        },
+
+        toggleAudio(audio) {
+            if (this.currentTrack != audio) {
+                console.log('changing track')
+                // this.$refs.player.src = audio
+                this.currentTrack = audio
+
+                this.isPlaying = true
+                this.$refs.player.addEventListener('canplay', () => {
+                    this.$refs.player.play()
+                })
+            } else {
+                console.log('pausing')
+                this.$refs.player.pause()
+                this.isPlaying = false
+            }
+            console.log(this.currentTrack, audio)
+        },
+
+        dateInFuture(date) {
+            let parsedDate = new Date(date)
+            let now = new Date()
+            return parsedDate > now ? true : false
+        },
+
+        getTime(time) {
+            // Hours, minutes and seconds
+            var hrs = ~~(time / 3600)
+            var mins = ~~((time % 3600) / 60)
+            var secs = ~~time % 60
+
+            // Output like "1:01" or "4:03:59" or "123:03:59"
+            var ret = '('
+            if (hrs > 0) {
+                ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
+            }
+            ret += '' + mins + ':' + (secs < 10 ? '0' : '')
+            ret += '' + secs
+            ret += ')'
+            return ret
+        }
     }
-  };
 }
--- src/data/posts.js
+++ src/data/posts.js
@@ -1,7 +1,14 @@
-const fetchData = require('../../utils/helpers/fetchData.js');
+const fetchData = require('../../utils/helpers/fetchData.js')
 
-const endPoint = 'https://public-api.wordpress.com/wp/v2/sites/eastmarshunited.wordpress.com/posts?per_page=20&_embed';
+const d = new Date()
+d.setMonth(d.getMonth() - 3)
+const threeMonthsAgo = d.toISOString()
+
+const endPoint =
+    process.env.CONTEXT == 'dev'
+        ? `https://public-api.wordpress.com/wp/v2/sites/eastmarshunited.wordpress.com/posts?after=${threeMonthsAgo}&per_page=10&_embed`
+        : 'https://public-api.wordpress.com/wp/v2/sites/eastmarshunited.wordpress.com/posts?per_page=50&_embed'
 
 module.exports = async function fetchPosts() {
-	return fetchData('posts', endPoint);
-};
+    return fetchData('posts', endPoint)
+}
